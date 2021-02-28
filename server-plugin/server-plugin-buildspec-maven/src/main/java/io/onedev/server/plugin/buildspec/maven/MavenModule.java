a8dbde0d487b81fb0598717bb005cd83a4314091
syphax ait yahia <syphax.ait-yahia@polymtl.ca>
2021-02-28 18:56:22 -0500

    allo

--- server-plugin/server-plugin-buildspec-maven/src/main/java/io/onedev/server/plugin/buildspec/maven/MavenModule.java
+++ server-plugin/server-plugin-buildspec-maven/src/main/java/io/onedev/server/plugin/buildspec/maven/MavenModule.java
@@ -24,7 +24,7 @@ public class MavenModule extends AbstractPluginModule {
 
 			@Override
 			public GroovyScript getScript() {
-				//comment
+				//comment hh
 				GroovyScript script = new GroovyScript();
 				script.setName(MavenJobSuggestion.DETERMINE_DOCKER_IMAGE);
 				script.setContent(Lists.newArrayList("io.onedev.server.plugin.buildspec.maven.MavenJobSuggestion.determineDockerImage()"));
